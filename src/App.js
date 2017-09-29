d78620c06ac8f62326f05dd5ec7d3c4cf7dd645b
James Calfee <james@jcalfee.info>
2017-09-29 13:40:11 -0500

    Simplify password form html

--- src/App.js
+++ src/App.js
@@ -376,7 +376,6 @@ class EnterPasswordForm extends React.Component {
   submit = ({password, hint = 'empty'}) => {
     // https://github.com/christianalfoni/formsy-react/issues/484
     const passValue = this.passwordRef.element.value
-
     this.props.onSubmit({password: passValue, hint})
   }
 
@@ -392,53 +391,46 @@ class EnterPasswordForm extends React.Component {
         <br />
 
         <Form onValidSubmit={this.submit}>
-          <fieldset>
-            <legend>Passphrase (optional)</legend>
-            <br />
-
-            {/* https://stackoverflow.com/questions/12374442/chrome-browser-ignoring-autocomplete-off */}
-            <input style={{display:'none'}}/>
-            <input type="password" style={{display:'none'}}/>
-
-            <Input type="password" name="password" id="password"
-              label="Passphrase" autoComplete="off" placeholder="Password"
-              componentRef={component => {this.passwordRef = component}}
-            />
-
-            {/* https://github.com/christianalfoni/formsy-react/issues/484
-            <Input
-              type="password" name="confirm" label="Confirm"
-              autoComplete="off" placeholder="Confirm"
-              validations="equalsField:password"
-              validationErrors={{equalsField: 'Passwords must match.'}}
-            />*/}
-
-            <ul>
-              <li>Every passphrase creates a different wallet</li>
-              <li>Every passphrase is valid (including no passphrase)</li>
-              <li>This passphrase acts like an additional word added the Mnemonic Key</li>
-              <li>Consider a passphrase you can share with those you trust</li>
-              <li>No passphrase, no private key, no funds</li>
-              <li>Password is case sensitive</li>
-            </ul>
-          </fieldset>
+          <h3>Passphrase (optional)</h3>
           <br />
 
-          <fieldset>
-            <legend> Hint (recommended)</legend>
-            This will appear on your backup.  The passphrase is very important,
-            this hint is recommended.
-            <br />
-            <br />
+          {/* https://stackoverflow.com/questions/12374442/chrome-browser-ignoring-autocomplete-off */}
+          <input style={{display:'none'}}/>
+          <input type="password" style={{display:'none'}}/>
 
-            <Input name="hint" label="Passphrase Hint" />
-          </fieldset>
-          <br />
-
-          <input
-            className="btn btn-primary"
-            type="submit" defaultValue="Submit"
+          <Input type="password" name="password" id="password"
+            label="Passphrase" autoComplete="off" placeholder="Password"
+            componentRef={component => {this.passwordRef = component}}
           />
+
+          {/* https://github.com/christianalfoni/formsy-react/issues/484
+          <Input
+            type="password" name="confirm" label="Confirm"
+            autoComplete="off" placeholder="Confirm"
+            validations="equalsField:password"
+            validationErrors={{equalsField: 'Passwords must match.'}}
+          />*/}
+
+          <ul>
+            <li>Every passphrase creates a different wallet</li>
+            <li>Every passphrase is valid (including no passphrase)</li>
+            <li>This passphrase acts like an additional word added the Mnemonic Key</li>
+            <li>Consider a passphrase you can share with those you trust</li>
+            <li>No passphrase, no private key, no funds</li>
+            <li>Password is case sensitive</li>
+          </ul>
+          <br />
+
+          <h3> Hint (recommended)</h3>
+          This will appear on your backup.  The passphrase is very important,
+          this hint is recommended.
+          <br />
+          <br />
+
+          <Input name="hint" label="Passphrase Hint" placeholder="Hint" />
+          <br />
+
+          <input className="btn btn-primary" type="submit" defaultValue="Submit" />
         </Form>
       </fieldset>
     )
