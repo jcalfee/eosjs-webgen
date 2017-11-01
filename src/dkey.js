9608c53ba5325cfab2167d83674b6a042a02a663
James Calfee <james@jcalfee.info>
2017-11-01 11:49:55 -0500

    Updated derived key docs and simplified algorithm

--- src/dkey.js
+++ src/dkey.js
@@ -2,17 +2,21 @@ const assert = require('assert')
 const createHash = require('create-hash')
 const createHmac = require('create-hmac')
 
+/** Derived Key Creator */
 module.exports = DKey
 
 /**
-  @example DKey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic, passphrase))
+  @example dkey = DKey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic, passphrase))
+
+  @example dkey('coin/account/role')
+  @example dkey('eos/account/owner')
+  @example dkey('eos/account/active')
+
   @return {function(path)} returns sha256(seed + path)
 */
 function DKey(seed) {
   assert(Buffer.isBuffer(seed), 'Buffer', 'buffer seed required')
-  assert.equal(seed.length, 64, 'seed should be 512 bytes')
-
-  const IL = seed.slice(0, 32)
+  assert.equal(seed.length, 32, 'seed should be 256 bytes')
 
   return path => {
     assert.equal(typeof path, 'string', 'string path required')
@@ -20,19 +24,15 @@ function DKey(seed) {
     assert(path[0] !== '/', 'path should not start with a slash')
     assert(path[path.length - 1] !== '/', 'path should not end with a slash')
     assert(!/[A-Z]/.test(path), 'path should not have uppercase letters')
-    return createHash('sha256').update(IL).update(path).digest()
+    return createHash('sha256').update(seed).update(path).digest()
   }
 }
 
-/**
-  @example DKey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic, passphrase))
-  @return {function(path)} returns sha256(seed + path)
-*/
 DKey.fromMasterSeed = function(seedBuffer) {
   assert(Buffer.isBuffer(seedBuffer), 'Buffer', 'buffer seed required')
   assert.equal(seedBuffer.length, 64, 'seed should be 512 bytes')
 
   const MASTER_SALT = 'Deterministic seed'
-  const seed = createHmac('sha512', MASTER_SALT).update(seedBuffer).digest()
+  const seed = createHmac('sha256', MASTER_SALT).update(seedBuffer).digest()
   return DKey(seed)
 }
